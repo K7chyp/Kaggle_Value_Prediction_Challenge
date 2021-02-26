d73b5b669f8d88557a709b2bec128818dbf413f1
K7chyp <nekitos199@gmail.com>
2021-02-26 18:34:40 +0300

    add module

---
+++ Preprocessing.py
@@ -0,0 +1,25 @@
+class Preprocessing:
+    def __init__(self, train_df, test_df):
+        self.train_ = train_df.copy()
+        self.test_ = test_df.copy()
+
+        self.remove_constant_columns()
+        self.drop_sparse()
+
+    def remove_constant_columns(self):
+
+        col_rm = []
+        for col in self.train_.columns:
+            if col != "ID" and col != "target":
+                if self.train_[col].std() == 0:
+                    col_rm.append(col)
+
+        self.train_.drop(col_rm, axis=1, inplace=True)
+        self.test_.drop(col_rm, axis=1, inplace=True)
+
+    def drop_sparse(self):
+        flist = [x for x in self.train_.columns if not x in ["ID", "target"]]
+        for f in flist:
+            if len(np.unique(self.train_[f])) < 2:
+                self.train_.drop(f, axis=1, inplace=True)
+                self.test_.drop(f, axis=1, inplace=True)
